c9237bc92238d021c4a2703a2ed2f678f98e2008
hyein <poptarts@kakao.com>
2022-10-19 14:35:39 +0900

    Refactor: Axios interceptors 적용

--- src/api/request.js
+++ src/api/request.js
@@ -2,13 +2,16 @@ import axios from "axios";
 
 const token = localStorage.getItem("accesstoken");
 
-const headers = { headers: { Authorization: token } };
-
 export const instance = axios.create({
   baseURL: `https://pre-onboarding-selection-task.shop`,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+instance.interceptors.request.use(function (config) {
+  if (token) config.headers.Authorization = token;
+  return config;
 });
 
 export const signUp = async (body) => {
@@ -20,17 +23,17 @@ export const signIn = async (body) => {
 };
 
 export const createTodo = async (todo) => {
-  return await instance.post(`/todos`, { todo }, headers);
+  return await instance.post(`/todos`, { todo });
 };
 
 export const getTodo = async () => {
-  return await instance.get(`/todos`, headers);
+  return await instance.get(`/todos`);
 };
 
 export const updateTodo = async (id, todo, isCompleted) => {
-  return await instance.put(`/todos/${id}`, { todo, isCompleted }, headers);
+  return await instance.put(`/todos/${id}`, { todo, isCompleted });
 };
 
 export const deleteTodo = async (id) => {
-  return await instance.delete(`/todos/${id}`, headers);
+  return await instance.delete(`/todos/${id}`);
 };
